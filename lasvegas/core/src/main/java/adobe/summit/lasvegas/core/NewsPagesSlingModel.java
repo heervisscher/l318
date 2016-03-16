06a80736c0735974526959ff82ad5a78c8ccff26
Feike Visser <fvisser@adobe.com>
2016-03-16 20:37:08 +0100

    Empty SlingModel file

---
+++ lasvegas/core/src/main/java/adobe/summit/lasvegas/core/NewsPagesSlingModel.java
@@ -0,0 +1,13 @@
+package adobe.summit.lasvegas.core;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+
+
+public class NewsPagesSlingModel {
+	
+}
