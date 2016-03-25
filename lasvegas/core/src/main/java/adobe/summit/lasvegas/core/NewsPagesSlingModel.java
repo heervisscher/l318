1551a3f5fbc94bba698817931d6bf86ecfa73fed
Feike Visser <fvisser@adobe.com>
2016-03-25 17:49:07 -0400

    Using selectors and sling-models to render pages, and select the first image and text component.

--- lasvegas/core/src/main/java/adobe/summit/lasvegas/core/NewsPagesSlingModel.java
+++ lasvegas/core/src/main/java/adobe/summit/lasvegas/core/NewsPagesSlingModel.java
@@ -1,13 +1,39 @@
 package adobe.summit.lasvegas.core;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.Optional;
 
 
+@Model(adaptables=Resource.class)
 public class NewsPagesSlingModel {
+	
+	@Inject @Optional
+	List<Resource> par; // select all Resources under the "par" node
+	
+	public Resource textResource;
+	
+	public Resource imageResource;
+	
+	@PostConstruct
+	protected void init() {
+		if ( par != null ) {
+			// looping through all resource and check the first image and text component
+			par.forEach( resource -> selectResource(resource));
+		}
+	}
+	
+	private void selectResource(Resource r) {
+		if ( textResource == null && r.getResourceType().endsWith("text")) {
+			textResource = r;
+		} else if ( imageResource == null && r.getResourceType().endsWith("image") ) {
+			imageResource = r;
+		}
+	}
 	
 }
